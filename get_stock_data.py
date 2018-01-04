ecc92bd539ede4dba5a8d1eda5bc5c56d2a4b291
Kzone-m <kazune.miyagi92@gmail.com>
2018-01-04 12:13:27 +0900

    Create get_stock_data.py

---
+++ get_stock_data.py
@@ -0,0 +1,9 @@
+#  http://nekoyukimmm.hatenablog.com/entry/2016/12/27/221649
+from pandas_datareader.data import get_data_yahoo
+import datetime
+
+s = datetime.datetime(2017, 1, 4)
+e = datetime.datetime(2017, 12, 29)
+
+df = get_data_yahoo(symbols='^N225', start=s, end=e)
+df.to_csv("stock2017_1_4_to_12_29.csv")
